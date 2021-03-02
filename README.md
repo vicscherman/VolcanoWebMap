b44ce3a855c38ca112ee2de472fa46ea5d81b3cd
vicscherman <vicscherman@gmail.com>
2021-03-02 16:00:03 -0500

    deployed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VolcanoWebMap
 An interactive python app that uses folium and pandas to show volcano locations in the USA
+To view the deployed application go to https://volcanowebmap.herokuapp.com
--- map1.py
+++ map1.py
@@ -1,5 +1,16 @@
 import folium
 import pandas
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("Map1.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
 
 data = pandas.read_csv("Volcanoes.txt")
 
